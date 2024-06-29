6aa9c2c197f2717795f366d9e053e18e9f740974
Liran Tal <liran.tal@gmail.com>
2024-06-29 11:17:08 +0300

    feat: update code to analyze the GitHub release notes for the Node.js proejct

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
-import bs4
+from bs4 import BeautifulSoup, SoupStrainer
 
 # Load environment variables from .env file
 load_dotenv()
 
-## LLM Model
+
+## Config setup
 CONFIG_MODEL="gpt-3.5-turbo-0125"
+WEBSITE_CRAWL="https://github.com/nodejs/node/releases/latest"
+
+## LLM Model
 llm = ChatOpenAI(model=CONFIG_MODEL)
 
 # Initialize the HuggingFace embeddings (if we want to use offline embeddings instead of the OpenAI one)
@@ -25,15 +29,24 @@ hf_embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-Mini
 
 ## Indexing
 ### Loaders
-# Only keep post title, headers, and content from the full HTML.
-bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+
 # Define the USER_AGENT header
 user_agent = "Mozilla/5.0 (compatible; MyLLMCrawler/1.0; +http://mywebsite.com/bot-info)"
 headers = {
     "User-Agent": user_agent
 }
+
+# Examples for filters:
+# bs4_strainer = bs4.SoupStrainer(class_=("markdown-body my-3"))
+# bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+
+bs4_strainer = SoupStrainer(lambda name, attrs: 
+    (name == "div" and attrs.get('class') and "markdown-body" in attrs['class']) or 
+    (name == "title")
+)
+
 loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    web_paths=(WEBSITE_CRAWL,),
     bs_kwargs={"parse_only": bs4_strainer},
     header_template={
       'User-Agent': user_agent
@@ -41,6 +54,7 @@ loader = WebBaseLoader(
 docs = loader.load()
 
 # print(len(docs[0].page_content))
+# print(docs[0].page_content)
 
 ### Splitters
 text_splitter = RecursiveCharacterTextSplitter(
@@ -49,10 +63,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 all_splits = text_splitter.split_documents(docs)
 
 # print(len(all_splits))
-# # also print the first split
 # print(all_splits[0])
-# print('-------------------')
-# print('\n')
 # print(all_splits[-1])
 
 ### Stores
@@ -72,10 +83,9 @@ retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-
 template = """Use the following context in XML tags to answer the question in XML tags.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible. You don't need to wrap the answer in XML tags.
+Keep the answer as concise as possible. You don't need to wrap the answer in XML tags.
 
 <context>
 {context}
@@ -103,10 +113,7 @@ rag_chain = (
     | StrOutputParser()
 )
 
-# result = rag_chain.invoke("What is Task Decomposition?")
-# print(result)
-
-for chunk in rag_chain.stream("What is Task Decomposition?"):
+for chunk in rag_chain.stream("What are the top 5 most significant changes in this release? (such as potential security updates, major API updates, or breaking changes)"):
     print(chunk, end="", flush=True)
 
 
